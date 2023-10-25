c49f4feec2b990d7416fb53824e7e41f2ed7ae36
Antoni1830 <122470326+Antoni1830@users.noreply.github.com>
2023-10-25 11:58:42 -0400

    Add files via upload

---
+++ scripts/getDates.js
@@ -0,0 +1,26 @@
+ const todaysdate= new Date();
+const options= {weekday:'long',day:'numeric',month:'long',year:'numeric'};
+document.getElementById('currentdate').textContent=new Date().toLocaleDateString('en-US',options)
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+const visitsDisplay = document.querySelector(".visits");
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+
+numVisits++;
+
+localStorage.setItem("numVisits-ls", numVisits);
+
+
