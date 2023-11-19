7a6f8c0ffc2067036192410f0ea9d3fa3ca571e4
Antoni1830 <122470326+Antoni1830@users.noreply.github.com>
2023-11-19 01:35:36 -0500

    Delete chamber directory

--- chamber/scripts/windchill.js
+++
@@ -1,5 +0,0 @@
-const temperature= 29
-const velocidad= 5
-const TWC= 35.74+(0.6215*temperature)-(35.75*velocidad+0.16)+(0.4215*temperature*velocidad+0.16)
-
-document.getElementById('Clima').textContent=TWC
